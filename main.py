220d6f5073b442d87c08d55e81b85813df12c95d
Bakalhau <bakalhausalgado@proton.me>
2025-01-10 21:31:12 -0300

    Adding config file

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import json
 
 # Load environment variables
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
+def load_config():
+    with open('config.json', 'r') as f:
+        return json.load(f)
+
 class FapBot(commands.Bot):
-    def __init__(self):
+    def __init__(self, config):
         intents = discord.Intents.default()
         intents.message_content = True
         intents.guilds = True
         intents.members = True
         
-        super().__init__(command_prefix='&', intents=intents)
+        self.config = config
+        super().__init__(command_prefix=config['prefix'], intents=intents)
     
     async def setup_hook(self):
         # Load all cogs
@@ -27,8 +33,20 @@ class FapBot(commands.Bot):
     async def on_ready(self):
         print(f'Bot connected as {self.user}')
 
+    async def on_message(self, message):
+        # Ignore messages from bots
+        if message.author.bot:
+            return
+
+        # Check if the message is in an allowed channel
+        if message.channel.id not in self.config['allowed_channels']:
+            return
+
+        await self.process_commands(message)
+
 def main():
-    bot = FapBot()
+    config = load_config()
+    bot = FapBot(config)
     bot.run(TOKEN)
 
 if __name__ == "__main__":
